b972295bdabf462bb671bcab9eacff37bb0fc94a
caden-sprague <caden.m.sprague@gmail.com>
2025-01-28 10:47:00 -0600

    Finish

---
+++ Desktop/CS/OOP/GitAndPackages/myUtil/myPackageMain.java
@@ -0,0 +1,12 @@
+package myUtil;
+
+public class myPackageMain {
+    public static void main(String[] args) {
+        int[] myArray = {1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println("The average of array is " + statistics.getAverage(myArray));
+
+        System.out.println("The median of array is " + statistics.getMedian(myArray));
+
+    }
+}
